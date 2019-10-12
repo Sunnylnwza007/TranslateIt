1034f810f62d05d1ad60f8d69b7e61216770f390
Sunny <trinnakrit@gmail.com>
2019-10-12 17:49:39 +0700

    add mainPage,fragment and databinding

---
+++ app/src/main/java/buu/informatics/s59160090/translateit/MainActivity.kt
@@ -0,0 +1,14 @@
+package buu.informatics.s59160090.translateit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import buu.informatics.s59160090.translateit.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/buu/informatics/s59160090/translateit/MainPage.kt
@@ -0,0 +1,25 @@
+package buu.informatics.s59160090.translateit
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import buu.informatics.s59160090.translateit.databinding.FragmentMainPageBinding
+
+class MainPage : Fragment() {
+    private lateinit var binding: FragmentMainPageBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentMainPageBinding>(inflater,
+            R.layout.fragment_main_page,container,false)
+
+        return binding.root
+    }
+
+
+}
